3cf494d9d141dccac75c1b86bd6801b4f7d67054
ahmedMshaban <ahmed.ms.elias@gmail.com>
2022-06-09 08:11:56 +0300

    init commit

---
+++ README.MD
@@ -0,0 +1,1 @@
+# Advisorbot
---
+++ main.cpp
@@ -0,0 +1,13 @@
+//
+//  main.cpp
+//  Advisorbot
+//
+//  Created by Ahmed Shaban on 9.06.2022.
+//
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!\n";
+    return 0;
+}
